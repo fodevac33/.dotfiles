b3edf2a222405d71295803422bda4f430c8f14a4
fodevac <fodevac@gmail.com>
2023-05-08 15:00:40 -0500

    cmp mappings

--- nvim/lua/fodevac/plugins/lsp.lua
+++ nvim/lua/fodevac/plugins/lsp.lua
@@ -7,24 +7,21 @@ lsp.ensure_installed({
 	"rust_analyzer",
 })
 
--- Fix Undefined global 'vim'
+-- fix undefined global 'vim'
 lsp.nvim_workspace()
 
--- local cmp = require("cmp")
--- local cmp_select = { behavior = cmp.SelectBehavior.Select }
--- local cmp_mappings = lsp.defaults.cmp_mappings({
--- 	["<C-p>"] = cmp.mapping.select_prev_item(cmp_select),
--- 	["<C-n>"] = cmp.mapping.select_next_item(cmp_select),
--- 	["<C-y>"] = cmp.mapping.confirm({ select = true }),
--- 	["<C-Space>"] = cmp.mapping.complete(),
--- })
---
--- cmp_mappings["<Tab>"] = nil
--- cmp_mappings["<S-Tab>"] = nil
---
--- lsp.setup_nvim_cmp({
--- 	mapping = cmp_mappings,
--- })
+local cmp = require("cmp")
+local cmp_mappings = lsp.defaults.cmp_mappings({
+	["<C-y>"] = cmp.mapping.confirm({ select = true }),
+	["<C-n>"] = cmp.mapping.complete(),
+})
+
+cmp_mappings["<Tab>"] = nil
+cmp_mappings["<S-Tab>"] = nil
+
+lsp.setup_nvim_cmp({
+	mapping = cmp_mappings,
+})
 
 lsp.set_preferences({
 	suggest_lsp_servers = false,
